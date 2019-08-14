43bb7891b819a60fe170f1411a44e483dfc4e8a0
QBC <1151379691@qq.com>
2019-08-14 09:12:38 +0800

    dilist修改

--- src/components/homeRecommend/homeRecommend.vue
+++ src/components/homeRecommend/homeRecommend.vue
@@ -3,7 +3,13 @@
     <HotImg :hotimg="hotimg"></HotImg>
     <HomeBar homeBarHot="精品好物" describe="等你来抢" more="true"></HomeBar>
     <div class="boutique" @click="showcommodity">
-      <DlList v-for="(item,index) in homeLi" :key="index" :item="item"></DlList>
+      <DlList 
+            v-for="(item,index) in homeLi" 
+            :key="index"
+            :img="item.imgUrl"
+            :title="item.title"
+            :price="item.vipPrice"
+          ></DlList>
     </div>
   </div>
 </template>
