a91188fbbb8399fd320a5ab204591d4d5e5b92eb
yjj <yujuanjuan107@163.com>
2019-08-18 21:33:29 +0800

    完成分享功能

--- dist/wx/pages/share/main.js
+++ dist/wx/pages/share/main.js
@@ -109,8 +109,6 @@ if (false) {(function () {
 //
 //
 //
-//
-//
 
 
 /* harmony default export */ __webpack_exports__["a"] = ({
@@ -121,24 +119,44 @@ if (false) {(function () {
                 nickName: '姬小胜是小狗',
                 code: 'C5JSKWUU2888',
                 goodsUrl: "https://img.alicdn.com/imgextra/i3/743750137/O1CN01nPCvEA1CsmoHlOLWB_!!0-item_pic.jpg_430x430q90.jpg",
-                title: "美宝莲星钻小灯管口红女持久保湿滋润显白金鱼花火唇膏夏日限量版",
+                title: "美宝莲星钻小灯管口红女持久保湿滋润显白金鱼花火唇膏夏日限量版2美宝莲星钻小灯管口红女持久保湿滋润显白金鱼花火唇膏夏日限量版",
                 vipPrice: "299",
-                salesPrice: "399",
-                qrCode: 'https://upapi.jinaup.com/static/image/QRCode.png'
+                salesPrice: "399"
+                // qrCode: 'https://upapi.jinaup.com/static/image/QRCode.png'
             }
         };
     },
 
     computed: __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_extends___default()({}, Object(__WEBPACK_IMPORTED_MODULE_1_vuex__["d" /* mapState */])({
-        goodsDetail: function goodsDetail(state) {
-            return state.productDetail.goodsDetail;
-        }
+        // userInfo: state=>state.productDetail.userInfo
     })),
-    methods: {},
+    methods: {
+        //点击保存图片
+        save: function save() {
+            wx.canvasToTempFilePath({
+                canvasId: "shareCanvas",
+                quality: 1,
+                complete: function complete(res) {
+                    console.log(res);
+                    if (res.errMsg == "canvasToTempFilePath:ok") {
+                        wx.saveImageToPhotosAlbum({
+                            filePath: res.tempFilePath,
+                            complete: function complete(res) {
+                                console.log("res", res);
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    },
     created: function created() {},
     mounted: function mounted() {
-        var context = wx.createCanvasContext('shareCanvas'); //绘制的上下文
+        var _this = this;
 
+        var context = wx.createCanvasContext('shareCanvas'); // 获取绘制图像的上下文
+        context.setFillStyle("white"); // 背景
+        context.fillRect(0, 0, 414, 736); //  你，、绘制一个矩形，大小是414,736  6plus的尺寸
         //绘制头像
         context.drawImage(this.userInfo.avtar, 10, 20, 50, 50);
         //绘制昵称
@@ -155,31 +173,54 @@ if (false) {(function () {
         context.restore();
 
         //绘制商品图片：
-        context.drawImage(this.goodsDetail.mainImgUrl, 60, 100, 200, 200);
+        context.restore();
+        wx.getImageInfo({ //获取图片的信息：
+            src: this.userInfo.goodsUrl,
+            success: function success(res) {
+                context.drawImage(_this.userInfo.goodsUrl, 0, 0, res.width, res.height, 19.7, 117, 275, 280);
+                context.draw(true);
+            }
+        });
 
         //绘制介绍文字：
         context.save();
+        context.setFillStyle("black");
         context.setFontSize(16);
-        context.fillText(this.goodsDetail.title + "顺丰包邮", 10, 340);
+        var row = 0;
+        var pos = 0;
+        for (var i = 0; i < this.userInfo.title.length; i++) {
+            var str = this.userInfo.title.slice(pos, i);
+            var len = context.measureText(str).width;
+            if (len >= 300 && len < 375) {
+                context.fillText(str, 20, 425 + row * 20);
+                row++;
+                pos = i;
+            }
+        }
+        if (pos < this.userInfo.title.length - 1) {
+            context.fillText(this.userInfo.title.slice(pos), 20, 425 + row * 20);
+        }
+        // context.fillText(this.userInfo.title+"顺丰包邮",0)
         context.setFillStyle("red");
         context.setFontSize(16);
-        context.fillText("￥" + this.goodsDetail.vipPrice + "元", 10, 370);
+        context.fillText("￥" + this.userInfo.vipPrice + "元", 10, 455 + row * 20);
         context.setFontSize(12);
         context.setFillStyle("#333");
-        context.fillText("￥" + this.goodsDetail.salesPrice + "元", 70 + context.measureText(this.goodsDetail.vipPrice).width, 370);
+        context.fillText("￥" + this.userInfo.salesPrice + "元", 70 + context.measureText(this.userInfo.vipPrice).width, 455 + row * 20);
         context.restore();
 
         //一条斜线
         context.save();
-        context.moveTo(70 + context.measureText(this.goodsDetail.vipPrice).width, 365);
-        context.lineTo(120 + context.measureText(this.goodsDetail.vipPrice).width, 365);
+        context.moveTo(70 + context.measureText(this.userInfo.vipPrice).width, 450 + row * 20);
+        context.lineTo(120 + context.measureText(this.userInfo.vipPrice).width, 450 + row * 20);
         context.stroke();
 
         //二维码文字和图片：
         context.save();
-        context.setFontSize(16);
-        context.fillText("长按识别二维码访问", 10, 420);
-        context.drawImage(this.userInfo.qrCode, 240, 360, 60, 60);
+        context.setFontSize(20);
+        context.setFillStyle("black");
+        context.fillText("长按识别二维码访问", 10, 490 + row * 20);
+        context.drawImage(this.userInfo.goodsUrl, 270, 470 + row * 20, 60, 60);
         context.restore();
 
         //生成图片：
@@ -200,17 +241,24 @@ var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._sel
     staticClass: "share"
   }, [_vm._m(0), _vm._v(" "), _c('ul', {
     staticClass: "footer"
-  }, [_c('li', [_c('img', {
+  }, [_c('li', {
+    attrs: {
+      "eventid": '0'
+    },
+    on: {
+      "click": _vm.save
+    }
+  }, [_c('img', {
     attrs: {
       "src": "/static/images/保存图片 copy 2.svg",
       "alt": ""
     }
-  }), _vm._v(" "), _c('span', [_vm._v("保存到相册")])]), _vm._v(" "), _c('li', [_c('img', {
+  })]), _vm._v(" "), _c('li', [_c('img', {
     attrs: {
       "src": "/static/images/分享微信.svg",
       "alt": ""
     }
-  }), _vm._v(" "), _c('span', [_vm._v("分享给朋友")])])], 1)], 1)
+  })])], 1)], 1)
 }
 var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
   return _c('div', {
--- src/pages/share/index.vue
+++ src/pages/share/index.vue
@@ -4,13 +4,11 @@
             <canvas canvas-id="shareCanvas" style="width: 100%; height:100%;"></canvas>
         </div>
         <ul class="footer">
-            <li>
+            <li @click="save">
                 <img src="/static/images/保存图片 copy 2.svg" alt="">
-                <span>保存到相册</span>
             </li>
             <li>
                 <img src="/static/images/分享微信.svg" alt="">
-                <span>分享给朋友</span>
             </li>
         </ul>
     </div>
@@ -25,27 +23,45 @@ export default {
                 nickName: '姬小胜是小狗',
                 code: 'C5JSKWUU2888',
                 goodsUrl:"https://img.alicdn.com/imgextra/i3/743750137/O1CN01nPCvEA1CsmoHlOLWB_!!0-item_pic.jpg_430x430q90.jpg",
-                title:"美宝莲星钻小灯管口红女持久保湿滋润显白金鱼花火唇膏夏日限量版",
+                title:"美宝莲星钻小灯管口红女持久保湿滋润显白金鱼花火唇膏夏日限量版2美宝莲星钻小灯管口红女持久保湿滋润显白金鱼花火唇膏夏日限量版",
                 vipPrice:"299",
                 salesPrice:"399",
-                qrCode: 'https://upapi.jinaup.com/static/image/QRCode.png'
+                // qrCode: 'https://upapi.jinaup.com/static/image/QRCode.png'
             }
         }
     },
     computed:{
         ...mapState({
-            goodsDetail: state=>state.productDetail.goodsDetail
+            // userInfo: state=>state.productDetail.userInfo
         })
     },
     methods:{
-
+        //点击保存图片
+        save(){
+            wx.canvasToTempFilePath({
+                canvasId: "shareCanvas",
+                quality:1,
+                complete: res=>{
+                    console.log(res)
+                    if(res.errMsg == "canvasToTempFilePath:ok"){
+                        wx.saveImageToPhotosAlbum({
+                            filePath: res.tempFilePath,
+                            complete: res=>{
+                                console.log("res",res)
+                            }
+                        })
+                    }
+                }
+            })
+        }
     },
     created(){
 
     },
     mounted(){
-        var context = wx.createCanvasContext('shareCanvas')   //绘制的上下文
-        
+        var context = wx.createCanvasContext('shareCanvas')   // 获取绘制图像的上下文
+        context.setFillStyle("white")  // 背景
+        context.fillRect(0,0,414,736)   //  你，、绘制一个矩形，大小是414,736  6plus的尺寸
         //绘制头像
         context.drawImage(this.userInfo.avtar, 10, 20, 50, 50);
         //绘制昵称
@@ -62,31 +78,55 @@ export default {
         context.restore();
 
         //绘制商品图片：
-        context.drawImage(this.goodsDetail.mainImgUrl,60,100,200,200)
+        context.restore();
+        wx.getImageInfo({   //获取图片的信息：
+            src:this.userInfo.goodsUrl,
+            success: res=>{
+                context.drawImage(this.userInfo.goodsUrl,0,0,res.width,res.height,19.7,117,275,280)
+                context.draw(true)
+            }
+        })
+       
 
         //绘制介绍文字：
         context.save()
+        context.setFillStyle("black")
         context.setFontSize(16)
-        context.fillText(this.goodsDetail.title+"顺丰包邮",10,340)
+        let row = 0;
+        let pos = 0;
+        for(let i =0; i<this.userInfo.title.length; i++){
+            let str = this.userInfo.title.slice(pos,i)
+            let len = context.measureText(str).width;
+            if(len >= 300 && len < 375){
+                context.fillText(str,20,425+row*20);
+                row++;
+                pos = i;
+            }
+        }
+        if(pos < this.userInfo.title.length-1){
+            context.fillText(this.userInfo.title.slice(pos), 20, 425+row*20)
+        }
+        // context.fillText(this.userInfo.title+"顺丰包邮",0)
         context.setFillStyle("red")
         context.setFontSize(16)
-        context.fillText("￥"+this.goodsDetail.vipPrice+"元",10,370)
+        context.fillText("￥"+this.userInfo.vipPrice+"元",10,455+row*20)
         context.setFontSize(12)
         context.setFillStyle("#333")
-        context.fillText("￥"+this.goodsDetail.salesPrice+"元",70+context.measureText(this.goodsDetail.vipPrice).width,370)
+        context.fillText("￥"+this.userInfo.salesPrice+"元",70+context.measureText(this.userInfo.vipPrice).width,455+row*20)
         context.restore()
 
         //一条斜线
         context.save()
-        context.moveTo(70+context.measureText(this.goodsDetail.vipPrice).width,365 );
-        context.lineTo(120+context.measureText(this.goodsDetail.vipPrice).width,365);
+        context.moveTo(70+context.measureText(this.userInfo.vipPrice).width,450+row*20 );
+        context.lineTo(120+context.measureText(this.userInfo.vipPrice).width,450+row*20);
         context.stroke();
 
         //二维码文字和图片：
         context.save()
-        context.setFontSize(16)
-        context.fillText("长按识别二维码访问",10,420)
-        context.drawImage(this.userInfo.qrCode,240,360,60,60)
+        context.setFontSize(20)
+        context.setFillStyle("black")
+        context.fillText("长按识别二维码访问",10,490+row*20)
+        context.drawImage(this.userInfo.goodsUrl,270,470+row*20,60,60)
         context.restore()
 
         //生成图片：
